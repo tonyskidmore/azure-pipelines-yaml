a1f77e0a032da52f380ae42faf45da04b415d7e0
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2022-12-03 18:33:07 +0000

    fixing variables

--- terraform/scripts/terraform-init.sh
+++ terraform/scripts/terraform-init.sh
@@ -9,4 +9,4 @@ printf "Terraform working directory: %s\n" "$tf_dir"
 # install git if not already installedgit status
 git --version || apt install git
 
-terraform init
+TF_IN_AUTOMATION=true terraform init
--- terraform/scripts/terraform-plan.sh
+++ terraform/scripts/terraform-plan.sh
@@ -6,4 +6,4 @@ tf_dir="$1"
 
 printf "Terraform working directory: %s\n" "$tf_dir"
 
-terraform plan -out tfplan
+TF_IN_AUTOMATION=true terraform plan -out tfplan
