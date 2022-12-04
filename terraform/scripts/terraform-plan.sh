b1f056d14b7b5439973952a5a91c851fc7fae62f
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2022-12-04 13:10:48 +0000

    adding plan condition

--- terraform/scripts/terraform-plan.sh
+++ terraform/scripts/terraform-plan.sh
@@ -6,4 +6,8 @@ tf_dir="$1"
 
 printf "Terraform working directory: %s\n" "$tf_dir"
 
-TF_IN_AUTOMATION=true terraform plan -out tfplan
+TF_IN_AUTOMATION=true terraform plan -out tfplan -input=false -detailed-exitcode
+exit_code=$?
+
+printf "Terraform exit code: %s\n" "$exit_code"
+echo "##vso[task.setvariable variable=tf_detailed_exit_code;isOutput=true]$exit_code"
