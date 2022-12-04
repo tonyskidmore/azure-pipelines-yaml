88ef645f2d2415916c2357e8e23cd44e90fbcdd6
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2022-12-04 13:42:57 +0000

    Updating plan

--- terraform/scripts/terraform-plan.sh
+++ terraform/scripts/terraform-plan.sh
@@ -11,3 +11,14 @@ exit_code=$?
 
 printf "Terraform exit code: %s\n" "$exit_code"
 echo "##vso[task.setvariable variable=tf_detailed_exit_code;isOutput=true]$exit_code"
+
+# if exit code is equal to 2 then changes are required
+# https://www.terraform.io/docs/cli/commands/plan.html#usage
+if [[ $exit_code -eq 2  ]]
+then
+  printf "Terraform detected required changes\n"
+  exit_code=0
+fi
+
+printf "Exit code: %s\n" "$exit_code"
+exit "$exit_code"
