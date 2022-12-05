ba25d225131389d9fccecaf27eff82f0c5c2dd9e
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2022-12-05 09:00:34 +0000

    updating destroy mode

--- terraform/scripts/terraform-init.sh
+++ terraform/scripts/terraform-init.sh
@@ -3,10 +3,17 @@
 set -euo pipefail
 
 tf_dir="$1"
+tf_destroy_local="$2"
 
 printf "Terraform working directory: %s\n" "$tf_dir"
 
 # install git if not already installedgit status
 git --version || apt install git
 
-TF_IN_AUTOMATION=true terraform init
+if [[ "${tf_destroy_local,,}" == "true" ]]
+then
+  rm "$tf_dir/backend.tf"
+  TF_IN_AUTOMATION=true terraform init -migrate-state -force-copy
+else
+  TF_IN_AUTOMATION=true terraform init
+fi
